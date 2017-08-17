b7992b9d600da604183a41b44b6abcc65242b129
woniu <275949034@qq.com>
2017-08-17 11:04:08 +0800

    跑exoplayer的demo

--- app/src/main/java/com/woniu/demo/App.kt
+++ app/src/main/java/com/woniu/demo/App.kt
@@ -1,6 +1,8 @@
 package com.woniu.demo
 
 import android.app.Application
+import com.google.android.exoplayer2.upstream.*
+import com.google.android.exoplayer2.util.Util
 
 /**
  * @author woniu
@@ -11,7 +13,23 @@ import android.app.Application
  * @since 2017/8/2 下午2:37
  */
 class App : Application() {
+
+    val userAgent: String by lazy { Util.getUserAgent(this, "ExoPlayerDemo") }
+
     override fun onCreate() {
         super.onCreate()
+    }
+
+    fun buildDataSourceFactory(bandwidthMeter: DefaultBandwidthMeter): DataSource.Factory {
+        return DefaultDataSourceFactory(this, bandwidthMeter,
+                buildHttpDataSourceFactory(bandwidthMeter))
+    }
+
+    fun buildHttpDataSourceFactory(bandwidthMeter: DefaultBandwidthMeter): HttpDataSource.Factory {
+        return DefaultHttpDataSourceFactory(userAgent, bandwidthMeter)
+    }
+
+    fun useExtensionRenderers(): Boolean {
+        return BuildConfig.FLAVOR.equals("withExtensions")
     }
 }
